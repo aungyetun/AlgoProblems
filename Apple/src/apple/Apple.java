41d129dbf920f6f248a130ba7cdfea20d5f2a463
aungyetun <aungthantzinwin@gmail.com>
2016-09-21 21:23:34 -0700

    Playing Stock(Greedy Metho)

--- Apple/src/apple/Apple.java
+++ Apple/src/apple/Apple.java
@@ -16,6 +16,39 @@ public class Apple {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int[] stockPricesYesterday = new int[]{10, 7, 5, 8, 11, 9};
+
+        System.out.println(MaxProfit(stockPricesYesterday));
+        
+    }
+    
+    
+    
+    /*
+    Apple stock - How much profit you can make from yesterdaystock
+    Buying at the lowest price and selling at the highest price
+    int[] stockPricesYesterday = new int[]{10, 7, 5, 8, 11, 9};
+    --- returns 6 (buying for $5 and selling for $11)
+    
+     O(n) time and O(1)space
+    */
+    public static int MaxProfit(int[] StockFromYesterday){
+        
+        if(StockFromYesterday.length <2){
+            throw new IllegalArgumentException("Need At Least 2 prices");
+        }
+        
+        // Greedy Method
+        int minprice = StockFromYesterday[0];
+        int maxprice = StockFromYesterday[0];
+        
+        for(int i =1;i<StockFromYesterday.length;i++){
+            int current = StockFromYesterday[i];
+            
+            minprice = Math.min(minprice,current);
+            maxprice = Math.max(maxprice, current);
+        }
+        return maxprice - minprice;
     }
     
 }
