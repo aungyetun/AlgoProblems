eb66d20bafc00789f0cf58a0ebc61d331c5bfbae
aungyetun <aungthantzinwin@gmail.com>
2016-09-27 13:14:18 -0700

    DiagonalDifference

--- Apple/src/apple/DiagonalDifference.java
+++ Apple/src/apple/DiagonalDifference.java
@@ -41,7 +41,7 @@ import java.util.*;
 public class DiagonalDifference {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+       
         Scanner scan = new Scanner(System.in);
         int size = scan.nextInt();
         List<Integer> array = new ArrayList<Integer> ();
